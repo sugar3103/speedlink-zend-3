db09cafa90d096a0b7ac6dabb3c7f253fd16bbe3
Paul Phuoc <paul.phuoc@itlvn.com>
2018-12-14 10:11:32 +0700

    Config RestApi

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,7 +11,18 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Zend\Session',
+    'Zend\Db',
+    'Zend\Cache',
+    'Zend\Form',
+    'Zend\InputFilter',
+    'Zend\Filter',
+    'Zend\Hydrator',
+    'Zend\Paginator',
     'Zend\Router',
     'Zend\Validator',
+    'DoctrineModule',
+    'DoctrineORMModule',
+    'RestApi',
     'Application',
 ];
