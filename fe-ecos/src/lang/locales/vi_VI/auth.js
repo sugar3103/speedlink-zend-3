033acfccd5f1b4e4fcfdfc5995dd721a5e44f32d
layton.luan <layton.luan@itlvn.com>
2019-02-18 10:59:45 +0700

    commit vi_langs and fix checking methods

---
+++ fe-ecos/src/lang/locales/vi_VI/auth.js
@@ -0,0 +1,14 @@
+module.exports = {
+  //auth - login
+  'login.welcome': 'Chào mừng đến với',
+  'login.note': 'Vui lòng sử dụng tài khoản của bạn để đăng nhập',
+  'login.username': 'Tên đăng nhập',
+  'login.password': 'Mật khẩu',
+  'login.forgot-password': 'Quên mật khẩu?',
+  'login.remember-me': 'Ghi nhớ đăng nhập',
+  'login.login': 'Đăng Nhập',
+  'login.register': 'Tạo tài khoản',
+  'login.login-again': 'Vui lòng đăng nhập lại để tiếp tục',
+  'login.username-error-empty': 'Tên đăng nhập không được bỏ trống',
+  'login.password-error-empty': 'Mật khẩu không được bỏ trống',
+}
---
+++ fe-ecos/src/lang/locales/vi_VI/menu.js
@@ -0,0 +1,24 @@
+module.exports = {
+  //menu
+  'menu.dashboard': 'Bảng điều khiển',
+  'menu.master-data': 'Master Data',
+  'menu.status': 'Trạng thái',
+  'menu.address': 'Địa chỉ',
+  'menu.address_code': 'Address Code',
+  'menu.address_country': 'Đất nước',
+  'menu.address_city': 'Thành phố',
+  'menu.address_district': 'Quận/Huyện',
+  'menu.address_ward': 'Phường/Xã',
+  'menu.customer_service': 'Service/Shipment Type',
+  'menu.cs_carrier': 'Carrier',
+  'menu.cs_service': 'ServiceShipment',
+  'menu.cs_shipment_type': 'Shipment Type',
+  'menu.networkport': 'Network Port',
+  'menu.branch': 'Branch',
+  'menu.hub': 'Hub',
+  'menu.brancharea': 'Branch Area',
+  //Menu Setting
+  'menu.setting': 'Cài đặt',
+  'menu.user': 'Người dùng',
+
+}
