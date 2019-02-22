734c5ded8e32526ecb287c7a1cc87514597f7582
Paul Phuoc <paul.phuoc@itlvn.com>
2019-02-22 17:16:32 +0700

    Add Code Module Core

---
+++ module/Core/src/Plugin/Factory/AccessPluginFactory.php
@@ -0,0 +1,22 @@
+<?php
+namespace Core\Plugin\Factory;
+
+use Core\Plugin\AccessPlugin;
+use OAuth\Service\RbacManager;
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
+
+/**
+ * This is the factory for AccessPlugin. Its purpose is to instantiate the plugin
+ * and inject dependencies into its constructor.
+ * @package Core\Plugin\Factory
+ */
+class AccessPluginFactory implements FactoryInterface {
+
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        $rbacManager = $container->get(RbacManager::class);
+
+        return new AccessPlugin($rbacManager);
+    }
+}
