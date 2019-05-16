5d44a4feb0d54a4112adcec71fbba31199193656
Marcelo Huy <marcelo.huy@itlvn.com>
2019-05-16 12:01:12 +0700

    fix bug validate

--- fe-ecos/src/components/PricingDomestic/Zone/validateActionForm.js
+++ fe-ecos/src/components/PricingDomestic/Zone/validateActionForm.js
@@ -2,7 +2,7 @@ const validate = (values) => {
     const errors = {};
     if (!values.name) {
       errors.name = 'pri_dom.validate-name-empty';
-    } else if (values.name.length < 4) {
+    } else if (values.name.length < 2) {
       errors.name = 'pri_dom.validate-name-minlength';
     } else if (values.name.length > 60) {
       errors.name = 'pri_dom.validate-name-maxlength';
@@ -10,7 +10,7 @@ const validate = (values) => {
 
     if (!values.name_en) {
       errors.name_en = 'pri_dom.validate-nameEn-empty';
-    } else if (values.name_en.length < 4) {
+    } else if (values.name_en.length < 2) {
       errors.name_en = 'pri_dom.validate-nameEn-minlength';
     } else if (values.name_en.length > 60) {
       errors.name_en = 'pri_dom.validate-nameEn-maxlength';
