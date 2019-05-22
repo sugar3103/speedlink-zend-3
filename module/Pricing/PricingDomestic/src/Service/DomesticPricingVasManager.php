f0210721436d956bd54b4f133ba269bd797da62b
Paul Phuoc <paul.phuoc@itlvn.com>
2019-05-22 14:23:47 +0700

    Update DomesticPricingVasManager.php

--- module/Pricing/PricingDomestic/src/Service/DomesticPricingVasManager.php
+++ module/Pricing/PricingDomestic/src/Service/DomesticPricingVasManager.php
@@ -2,6 +2,7 @@
 namespace PricingDomestic\Service;
 
 use PricingDomestic\Entity\DomesticPricingVas;
+use PricingDomestic\Entity\DomesticPricingVasSpec;
 use PricingDomestic\Servivce\DomesticPricingVasCityManager;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\ORMException;
@@ -44,145 +45,70 @@ class DomesticPricingVasManager {
      * Get List Domestic PricingVas By Condition
      */
 
-    public function getListDomesticPricingVasByCondition($start, $limit, $sortField, $sortDirection,$filters)
+    public function getListDomesticPricingVas(\PricingDomestic\Entity\DomesticPricing $pricing)
     {
-        $areas = [];
+        $pricingVas = [];
         $totalPricingVas = 0;
-        //get orm Domestic PricingVas
-        $ormPricingVas = $this->entityManager->getRepository(DomesticPricingVas::class)
-            ->getListDomesticPricingVasByCondition($start, $limit, $sortField, $sortDirection, $filters);
+
+        $ormPricingVas = $this->entityManager->getRepository(DomesticPricingVas::class)->findBy(['domestic_pricing' => $pricing->getId()]);
         if($ormPricingVas){
-            //set offset,limit
-            $ormPaginator = new ORMPaginator($ormPricingVas, true);
-            $ormPaginator->setUseOutputWalkers(false);
-            $totalPricingVas = $ormPaginator->count();
-            //get domestic area list
-            
-            $areas = $ormPaginator->getIterator()->getArrayCopy();
-            
-             foreach ($areas as &$area) {
-                $area['cities'] =  $this->domesticPricingVasCityManager->getPricingVasCity($area['id']);
-                $area['created_at'] =  ($area['created_at']) ? Utils::checkDateFormat($area['created_at'],'D M d Y H:i:s \G\M\T+0700') : '';
-                $area['updated_at'] =  ($area['updated_at']) ? Utils::checkDateFormat($area['updated_at'],'D M d Y H:i:s \G\M\T+0700') : '';
-                $area['full_name_created'] = trim($area['full_name_created']);
-                $area['full_name_updated'] = trim($area['full_name_updated']);
+            foreach ($ormPricingVas as $vas) {
+                $vasSpec = [];
+                if($vas->getType() == 1) {
+                    $pricingVasSpecs = $this->entityManager->getRepository(DomesticPricingVasSpec::class)->findBy([
+                        'domestic_pricing' => $pricing,
+                        'domestic_pricing_vas'=> $vas
+                    ]);
+
+                    if($pricingVasSpecs) {
+                        foreach ($pricingVasSpecs as $pricingVasSpec ) {
+                            $vasSpec[] = array(
+                                'form' => $pricingVasSpec->getFrom(),
+                                'to' => $pricingVasSpec->getTo(),
+                                'value' => $pricingVasSpec->getValue()
+                            );
+                        }
+                    }
+                }
+                
+
+                $pricingVas[] = array(
+                    'id' => $vas->getId(),
+                    'name' => $vas->getName(),
+                    'name_en' => $vas->getNameEn(),
+                    'formula' => $vas->getFormula(),
+                    'min'       => $vas->getMin(),
+                    'type'  => $vas->getType(),
+                    'spec'  => $vasSpec           
+                );
             }
         }
-        //set data user
+
         $dataPricingVas = [
-            'listPricingVas' => $areas,
+            'listPricingVas' => $pricingVas,
             'totalPricingVas' => $totalPricingVas,
         ];
         
         return $dataPricingVas;
     }
 
-    /**
-     * Add Domestic PricingVas
-     */
-    public function addPricingVas($data,$user)
+    public function addPricingVas($data, $user)
     {
-        $this->entityManager->beginTransaction();
-        try {
-            $domesticPricingVas = new DomesticPricingVas();
-            $domesticPricingVas->setName($data['name']);
-            $domesticPricingVas->setNameEn($data['name_en']);
-            $domesticPricingVas->setCreatedBy($user->id);
-            
-            $addTime = new \DateTime('now', new \DateTimeZone('UTC'));
-            $domesticPricingVas->setCreatedAt($addTime->format('Y-m-d H:i:s'));            
-            $this->getReferenced($domesticPricingVas,$user,'add');
-            
-            $this->entityManager->persist($domesticPricingVas);
-            $this->entityManager->flush();        
-            
-            $this->entityManager->commit();
+        if (is_array($data)) {
+            foreach ($data['data'] as $value) {
+                $this->entityManager->beginTransaction();
+                try {
 
-            if(isset($data['cities']) && count($data['cities']) > 0) {
-                foreach ($data['cities'] as $city ) {
-                    $this->domesticPricingVasCityManager->updatePricingVasCity($domesticPricingVas->getId(), $city, $user);
+                } catch (ORMException $e) {
+                    $this->entityManager->rollback();
+                    return false;
                 }
             }
-
-            return $domesticPricingVas;
-
-        } catch (ORMException $e) {
-            $this->entityManager->rollback();
-            return FALSE;
         }
     }
 
-    /**
-     * Update Domestic PricingVas
-     */
-    public function updatePricingVas($domesticPricingVas, $data,$user) {
-
-        $this->entityManager->beginTransaction();
-        try {
-            $domesticPricingVas->setName($data['name']);
-            $domesticPricingVas->setNameEn($data['name_en']);
-            $domesticPricingVas->setUpdatedBy($user->id);
-            
-            $addTime = new \DateTime('now', new \DateTimeZone('UTC'));
-            $domesticPricingVas->setUpdatedAt($addTime->format('Y-m-d H:i:s'));
-            $this->getReferenced($domesticPricingVas,$user,'update');
-            // apply changes to database.
-            $this->entityManager->flush();            
-            $this->entityManager->commit();
-
-            $this->domesticPricingVasCityManager->deletePricingVasCity($domesticPricingVas->getId());
-
-            if(isset($data['cities']) && count($data['cities']) > 0) {                
-                foreach ($data['cities'] as $city ) {
-                    $this->domesticPricingVasCityManager->updatePricingVasCity($domesticPricingVas->getId(), $city, $user);
-                }
-            }
-
-           return $domesticPricingVas;
-        }
-        catch (ORMException $e) {
-            $this->entityManager->rollback();
-            return FALSE;
-        }
-    }
-
-     /**
-     * Delete Domestic PricingVas
-     */
-    public function deletePricingVas($domesticPricingVas, $user) {
-
-        $this->entityManager->beginTransaction();
-        try {
-            $domesticPricingVas->setIsDeleted(1);
-            $domesticPricingVas->setUpdatedBy($user->id);
-            
-            $addTime = new \DateTime('now', new \DateTimeZone('UTC'));
-            $domesticPricingVas->setUpdatedAt($addTime->format('Y-m-d H:i:s'));
-            
-            // apply changes to database.
-            $this->entityManager->flush();
-            // $last_id = $rangeweight->getBranchId();
-            $this->entityManager->commit();
-           
-        }
-        catch (ORMException $e) {
-            $this->entityManager->rollback();
-            return FALSE;
-        }
-    }
-
-    private function getReferenced(&$domesticPricingVas, $user, $mode = '')
+    public function deletedPricingVas(\PricingDomestic\Entity\DomesticPricing $pricing)
     {
-        $user_data = $this->entityManager->getRepository(User::class)->find($user->id);
-        if ($user_data == null) {
-            throw new \Exception('Not found User by ID');
-        }
-
-        if ($mode == 'add') {
-            $domesticPricingVas->setJoinCreated($user_data);
-        }
-
-        $domesticPricingVas->setJoinUpdated($user_data);
-
+        $this->entityManager->getRepository(DomesticPricingVas::class)->deleteVas($pricing->getId());
     }
 }
