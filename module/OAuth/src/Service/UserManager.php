5f0d0278daff6ba35e751d9397441d222778b3e8
Paul Phuoc <paul.phuoc@itlvn.com>
2019-05-29 14:25:43 +0700

    Update UserManager.php

--- module/OAuth/src/Service/UserManager.php
+++ module/OAuth/src/Service/UserManager.php
@@ -1,30 +1,28 @@
 <?php
 namespace OAuth\Service;
 
-use OAuth\Entity\Role;
-use OAuth\Entity\User;
+use Core\Utils\Utils;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\ORMException;
+use Doctrine\ORM\Tools\Pagination\Paginator as ORMPaginator;
+use OAuth\Entity\Role;
+use OAuth\Entity\User;
 use Zend\Crypt\Password\Bcrypt;
-use Zend\Math\Rand;
-use Zend\View\Renderer\PhpRenderer;
-use Zend\Mime\Message as MimeMessage;
-use Zend\Mime\Part as MimePart;
 use Zend\Mail\Message;
 use Zend\Mail\Transport\Smtp as SmtpTransport;
 use Zend\Mail\Transport\SmtpOptions;
-
-use DoctrineORMModule\Paginator\Adapter\DoctrinePaginator as DoctrineAdapter;
-use Doctrine\ORM\Tools\Pagination\Paginator as ORMPaginator;
-use Zend\Paginator\Paginator;
-use Core\Utils\Utils;
+use Zend\Math\Rand;
+use Zend\Mime\Message as MimeMessage;
+use Zend\Mime\Part as MimePart;
+use Zend\View\Renderer\PhpRenderer;
 
 /**
  * This service is responsible for adding/editing users
  * and changing user password.
  * @package OAuth\Service
  */
-class UserManager {
+class UserManager
+{
 
     /**
      * @var EntityManager
@@ -66,16 +64,15 @@ class UserManager {
         $permissionManager,
         $viewRenderer,
         $config
-    )
-    {
+    ) {
         $this->entityManager = $entityManager;
         $this->roleManager = $roleManager;
         $this->permissionManager = $permissionManager;
         $this->viewRenderer = $viewRenderer;
         $this->config = $config;
-    }   
+    }
 
-    private function getReferenced(&$user, $data,$assignee, $mode = '')
+    private function getReferenced(&$user, $data, $assignee, $mode = '')
     {
         $user_data = $this->entityManager->getRepository(User::class)->find($assignee->id);
         if ($user_data == null) {
@@ -93,7 +90,8 @@ class UserManager {
      * @return User|bool
      * @throws \Exception
      */
-    public function addUser($data) {
+    public function addUser($data)
+    {
 
         // begin transaction
         $this->entityManager->beginTransaction();
@@ -128,11 +126,10 @@ class UserManager {
             $this->entityManager->commit();
 
             return $user;
-        }
-        catch (ORMException $e) {
+        } catch (ORMException $e) {
 
             $this->entityManager->rollback();
-            return FALSE;
+            return false;
         }
 
     }
@@ -144,12 +141,13 @@ class UserManager {
      * @throws \Exception
      * @return bool
      */
-    public function updateUser($user, $data, $assignee) {
-        
+    public function updateUser($user, $data, $assignee)
+    {
+
         // begin transaction
         $this->entityManager->beginTransaction();
         try {
-            
+
             $user->setUsername($data['username']);
             $user->setFirstName($data['first_name']);
             $user->setLastName($data['last_name']);
@@ -157,7 +155,7 @@ class UserManager {
             $user->setEmail($data['email']);
 
             $updatedTime = new \DateTime('now', new \DateTimeZone('UTC'));
-            
+
             $user->setUpdatedAt($updatedTime->format('Y-m-d H:i:s'));
             $user->setUpdatedBy($assignee->id);
             $this->getReferenced($user, $data, $assignee);
@@ -171,11 +169,10 @@ class UserManager {
             $this->entityManager->commit();
 
             return true;
-        }
-        catch (ORMException $e) {
+        } catch (ORMException $e) {
 
             $this->entityManager->rollback();
-            return FALSE;
+            return false;
         }
     }
 
@@ -187,7 +184,8 @@ class UserManager {
      * @throws \Exception
      */
 
-    private function assignRoles($user, $roleIds) {        
+    private function assignRoles($user, $roleIds)
+    {
         // remove old user role(s).
         $user->getRoles()->clear();
 
@@ -196,8 +194,9 @@ class UserManager {
             $role = $this->entityManager->getRepository(Role::class)
                 ->find($roleId);
 
-            if ($role == null)
+            if ($role == null) {
                 throw new \Exception('Not found role by ID');
+            }
 
             $user->addRole($role);
         }
@@ -207,14 +206,12 @@ class UserManager {
      * This method checks if at least on user presents, and if not, creates
      * 'Admin' user with username 'admin' and password 'P@ssw0rdM1c!'
      */
-    public function createAdminUserIfNotExists() {
+    public function createAdminUserIfNotExists()
+    {
         $user = $this->entityManager->getRepository(User::class)
             ->findOneBy([]);
-        
-        if ($user == null) {
-            $this->permissionManager->createDefaultPermissionsIfNotExist();
-            $this->roleManager->createDefaultRolesIfNotExist();
 
+        if ($user == null) {
             $user = new User();
             $user->setUsername('administrator');
             $user->setFirstName('Administrator');
@@ -230,14 +227,31 @@ class UserManager {
             $adminRole = $this->entityManager->getRepository(Role::class)
                 ->findOneByName('Administrator');
 
-            if ($adminRole == null)
+            if ($adminRole == null) {
                 throw new \Exception("Administrator role doesn't exist");
+            }
 
             $user->getRoles()->add($adminRole);
 
             $this->entityManager->persist($user);
             $this->entityManager->flush();
+
         }
+
+        $this->permissionManager->createDefaultPermissionsIfNotExist();
+        $this->roleManager->createDefaultRolesIfNotExist();
+        // assign user Administrator role
+        $adminRole = $this->entityManager->getRepository(Role::class)
+            ->findOneByName('Administrator');
+
+        if ($adminRole == null) {
+            throw new \Exception("Administrator role doesn't exist");
+        }
+
+        $user->getRoles()->add($adminRole);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
     }
 
     /**
@@ -246,12 +260,14 @@ class UserManager {
      * @param $password
      * @return bool
      */
-    public function validatePassword($user, $password) {
+    public function validatePassword($user, $password)
+    {
         $bcrypt = new Bcrypt();
         $passwordHash = $user->getPassword();
 
-        if ($bcrypt->verify($password, $passwordHash))
+        if ($bcrypt->verify($password, $passwordHash)) {
             return true;
+        }
 
         return false;
     }
@@ -263,9 +279,11 @@ class UserManager {
      * @param $user User
      * @throws \Exception
      */
-    public function generatePasswordResetToken($user) {
-        if ($user->getIsActive() != User::ACTIVE)
+    public function generatePasswordResetToken($user)
+    {
+        if ($user->getIsActive() != User::ACTIVE) {
             throw new \Exception("Cannot generate password reset token for inactive user " . $user->getUsername());
+        }
 
         // Generate a token.
         $token = Rand::getString(32, '0123456789abcdefghijklmnopqrstuvwxyz', true);
@@ -294,7 +312,7 @@ class UserManager {
         $bodyHtml = $this->viewRenderer->render(
             'user/email/reset-password-email',
             [
-                'passwordResetUrl' => $passwordResetUrl
+                'passwordResetUrl' => $passwordResetUrl,
             ]
         );
 
@@ -325,20 +343,24 @@ class UserManager {
      * @param $passwordResetToken
      * @return bool
      */
-    public function validatePasswordResetToken($email, $passwordResetToken) {
+    public function validatePasswordResetToken($email, $passwordResetToken)
+    {
         // find user by email.
         $user = $this->entityManager->getRepository(User::class)
             ->findOneByEmail($email);
 
-        if ($user == null || $user->getIsActive() != User::ACTIVE)
+        if ($user == null || $user->getIsActive() != User::ACTIVE) {
             return false;
+        }
 
         // check that token hash matches the token hash in our DB.
         $bcrypt = new Bcrypt();
         $tokenHash = $user->getPasswordResetToken();
 
-        if (!$bcrypt->verify($passwordResetToken, $tokenHash))
-            return false; // mismatch
+        if (!$bcrypt->verify($passwordResetToken, $tokenHash)) {
+            return false;
+        }
+        // mismatch
 
         // check that token was created not too long ago
         $tokenCreateAt = $user->getPasswordResetTokenCreatedAt();
@@ -346,8 +368,10 @@ class UserManager {
 
         $currentDate = strtotime('now');
 
-        if ($currentDate - $tokenCreateAt > 24*60*60)
-            return false; // expired
+        if ($currentDate - $tokenCreateAt > 24 * 60 * 60) {
+            return false;
+        }
+        // expired
 
         return true;
     }
@@ -355,16 +379,19 @@ class UserManager {
     /**
      * This method sets new password by password reset token.
      */
-    public function setNewPasswordByToken($email, $passwordResetToken, $newPassword) {
-        if ($this->validatePasswordResetToken($email, $passwordResetToken))
+    public function setNewPasswordByToken($email, $passwordResetToken, $newPassword)
+    {
+        if ($this->validatePasswordResetToken($email, $passwordResetToken)) {
             return false;
+        }
 
         // find user with the given email.
         $user = $this->entityManager->getRepository(User::class)
             ->findOneByEmail($email);
 
-        if ($user == null || $user->getIsActive() != User::ACTIVE)
+        if ($user == null || $user->getIsActive() != User::ACTIVE) {
             return false;
+        }
 
         // set new password for user
         $bcrypt = new Bcrypt();
@@ -389,18 +416,21 @@ class UserManager {
      * @throws \Doctrine\ORM\ORMException
      * @throws \Doctrine\ORM\OptimisticLockException
      */
-    public function changePassword($user, $data) {
+    public function changePassword($user, $data)
+    {
         $oldPassword = $data['old_password'];
 
         // check that old password is correct
-        if (!$this->validatePassword($user, $oldPassword))
+        if (!$this->validatePassword($user, $oldPassword)) {
             return false;
+        }
 
         $newPassword = $data['new_password'];
 
         // Check password length
-        if (strlen($newPassword) < 6 || strlen($newPassword) > 64)
+        if (strlen($newPassword) < 6 || strlen($newPassword) > 64) {
             return false;
+        }
 
         // Set new password for user
         $bcrypt = new Bcrypt();
@@ -424,16 +454,17 @@ class UserManager {
      * @return array
      * @throws ORMException
      */
-    public function getListUserByCondition($start,$limit,$sortField = '',$sortDirection = 'asc',$filters = []){
+    public function getListUserByCondition($start, $limit, $sortField = '', $sortDirection = 'asc', $filters = [])
+    {
 
-        $users     = [];
+        $users = [];
         $totalUser = 0;
 
         //get orm user
         $ormUser = $this->entityManager->getRepository(User::class)
             ->getListUserByCondition($start, $limit, $sortField, $sortDirection, $filters);
-        
-        if($ormUser){
+
+        if ($ormUser) {
             //set offset,limit
             $ormPaginator = new ORMPaginator($ormUser, true);
             $ormPaginator->setUseOutputWalkers(false);
@@ -442,16 +473,16 @@ class UserManager {
             //get user list
             $users = $ormPaginator->getIterator()->getArrayCopy();
 
-            foreach ($users as &$user) {//loop
+            foreach ($users as &$user) { //loop
                 //set status
-                $user['status'] = $user['is_active'];                
+                $user['status'] = $user['is_active'];
                 //set created_at
-                $user['created_at'] =  ($user['created_at']) ? Utils::checkDateFormat($user['created_at'],'D M d Y H:i:s \G\M\T+0700') : '';
-                $user['updated_at'] =  ($user['updated_at']) ? Utils::checkDateFormat($user['updated_at'],'D M d Y H:i:s \G\M\T+0700') : '';
+                $user['created_at'] = ($user['created_at']) ? Utils::checkDateFormat($user['created_at'], 'D M d Y H:i:s \G\M\T+0700') : '';
+                $user['updated_at'] = ($user['updated_at']) ? Utils::checkDateFormat($user['updated_at'], 'D M d Y H:i:s \G\M\T+0700') : '';
                 $user['full_name_created'] = trim($user['full_name_created']);
                 $user['full_name_updated'] = trim($user['full_name_updated']);
             }
-            
+
         }
 
         //set data user
@@ -459,7 +490,7 @@ class UserManager {
             'listUser' => $users,
             'totalUser' => $totalUser,
         ];
-        
+
         return $dataUser;
     }
 }
